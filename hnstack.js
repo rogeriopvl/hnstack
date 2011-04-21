0439b5fcc429489b476bf312569fadd1c5e9a654
rogeriopvl <rogeriopvl@gmail.com>
2011-04-21 11:45:30 +0100

    Fixed some code. Its not working properly though.

--- hnstack.js
+++ hnstack.js
@@ -1,17 +1,29 @@
-var entries = document.getElementsByClassName('title');
+window.addEventListener('load', function(){
+	var entries = document.getElementsByClassName('title');
 
-for (var i=0; i<entries.length; i+=2) {
-	var entryRow = entries[i].parentNode;
-	var entryId = entries[i].nextSibling.getElementsByTagName('a')[0].id;
+	for (var i=0; i<entries.length; i+=2) {
+		var entryRow = entries[i].parentNode;
+		var nextElem = entries[i].nextSibling;
 
-	if (localStorage[entryId]) {
-		entryRow.style.display = 'none';
-		// i have to remove also the author line
+		if (nextElem) {
+			var voteLink = nextElem.getElementsByTagName('a')[0];
+			if (voteLink) {
+				var entryId = voteLink.id;
+			}
+			else { continue; }
+		}
+		else { continue; }
+
+		if (localStorage[entryId] == "1") {
+			console.log('found '+entryId);
+			entryRow.style.display = 'none';
+			// i have to remove also the author line
+		}
+		else {
+			entryRow.addEventListener('click', function(){
+				localStorage[entryId] = 1;
+				//this.style.display = 'none'; // change this to only hide after x minutes
+			});
+		}
 	}
-
-	console.log(entryRow);
-	entryRow.addEventListener('click', function(){
-		localStorage[entryId] = 1;
-		this.style.display = 'none'; // change this to only hide after x minutes
-	});
-}
+});
