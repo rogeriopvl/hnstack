f189c95841d2165817ec3d13ace218b19f4c47e8
rogeriopvl <rogeriopvl@gmail.com>
2011-04-22 23:33:28 +0100

    woohoo, working version.

--- hnstack.js
+++ hnstack.js
@@ -1,29 +1,55 @@
-window.addEventListener('load', function(){
-	var entries = document.getElementsByClassName('title');
+/**
+ * This chrome extension walks through the hacker news website < http://news.ycombinator.com >
+ * and hides entries that have already been clicked on.
+ * The read entries are stored in the localStorage database with the key "hnstack_entries".
+ *
+ * @author Rogério Vicente < http://rogeriopvl.com >
+ * @version 0.1
+ * @license BSD (check LICENSE file)
+ */
+window.addEventListener("load", function(){
 
-	for (var i=0; i<entries.length; i+=2) {
-		var entryRow = entries[i].parentNode;
-		var nextElem = entries[i].nextSibling;
+	var tables = document.getElementsByTagName("table");
+	var itemsTable = tables[2];
+	itemsTable.id = "hn_items";
 
-		if (nextElem) {
-			var voteLink = nextElem.getElementsByTagName('a')[0];
-			if (voteLink) {
-				var entryId = voteLink.id;
+	var tableRows = itemsTable.getElementsByTagName("tr");
+
+	for (var i=0; i<tableRows.length; i++) {
+
+		var rowLinks = tableRows[i].getElementsByTagName("a");
+
+		if (rowLinks[0] && rowLinks[0].id.indexOf("up_") != -1) {
+			tableRows[i].id = rowLinks[0].id;
+
+			if (hasReadEntry(rowLinks[0].id)) {
+				tableRows[i].className = "to_remove";
+				if (tableRows[i+1]) {
+					tableRows[i+1].className = "to_remove";
+				}
 			}
-			else { continue; }
+			else {
+				tableRows[i].addEventListener("click", function(){
+					markEntryHasRead(this.id);
+					//this.style.display = "none";
+				});
+			}
 		}
 		else { continue; }
-
-		if (localStorage[entryId] == "1") {
-			console.log('found '+entryId);
-			entryRow.style.display = 'none';
-			// i have to remove also the author line
-		}
-		else {
-			entryRow.addEventListener('click', function(){
-				localStorage[entryId] = 1;
-				//this.style.display = 'none'; // change this to only hide after x minutes
-			});
-		}
 	}
 });
+
+/* Adds an entry to the localStorage string */
+function markEntryHasRead(id) {
+	if (localStorage["hnstack_entries"]) {
+		localStorage["hnstack_entries"] += (id+";");
+	}
+	else {
+		localStorage["hnstack_entries"] = id+";";
+	}
+}
+
+/* Checks if and entry is in the localStorage string */
+function hasReadEntry(id) {
+	return localStorage["hnstack_entries"] ? localStorage["hnstack_entries"].indexOf(id) != -1 : false;
+}
